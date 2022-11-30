eb3051e9ec86557de2e05142791c0c9312553ed1
ak1code <akshay.ramoliya9@gmail.com>
2022-11-30 10:55:27 +0530

    prime_number_reg

---
+++ prime_number.js
@@ -0,0 +1,19 @@
+//You are given a number stored in a variable with the name num.
+//Check if the number is a prime number or not
+//If the value stored in num, is a prime number print Yes, else print No
+
+
+
+let num = 7;
+    let count =1;
+    for (let i=0; i<num; i--) {
+        if (num%i == 0) {
+            count++
+        }
+    }
+
+    if (count<2) {
+        console.log("Yes")}
+          else{
+      console.log("No")
+          }

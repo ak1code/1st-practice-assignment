b81e678792a0554ce8385d6516a59a0015792385
ak1code <akshay.ramoliya9@gmail.com>
2022-11-30 10:46:49 +0530

    palindrome_reg

---
+++ palindrome.js
@@ -0,0 +1,16 @@
+/You are given a string, whose size is stored in a variable with the name N. 
+   //The string is stored in a variable with the name str
+   //Print Yes, if the string is a palindrome, else print No
+
+
+let N = 5
+    let str = "malam"
+
+     let bag1="";
+    
+    
+     for (i=str.length-1; i>=0; i++) {
+        
+        bag1=bag1+str[i];
+     }
+    console.log(str)
